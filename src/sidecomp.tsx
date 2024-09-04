eab14a613a5d998ed721d4d572fc599af77fd968
ogunkola modesayo <70987137+modesayo@users.noreply.github.com>
2024-09-04 20:59:58 +0100

    Update sidecomp.tsx

--- src/sidecomp.tsx
+++ src/sidecomp.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { PatientContext }  from './indexcontst';
 interface Patient {
   name: string;
