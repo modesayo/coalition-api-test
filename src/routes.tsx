2c2c42a009d4861d4cf4b7a027c877c3c492c1fd
ogunkola modesayo <70987137+modesayo@users.noreply.github.com>
2024-09-04 20:59:23 +0100

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import About from './about';
 import Contact from './contact';
